95b351b54cbdcbb5d1e9fdc80b1e1ca7fa2a6a0a
TheWanderingShinobi <thewanderingshinobi@proton.me>
2024-03-19 07:32:47 -0400

    Day 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
-import react from "@astrojs/react";
+import critters from "astro-critters";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), critters()]
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
 const { addDynamicIconSelectors } = require('@iconify/tailwind')
 
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Poppins', ...defaultTheme.fontFamily.sans]
+			}
+		}
+	},
 	plugins: [
 		require('daisyui'),
 		addDynamicIconSelectors(),
@@ -13,10 +21,10 @@ export default {
 		themes: [
 			{
 				mytheme: {
-					primary: '#a991f7',
-					secondary: '#f6d860',
-					accent: '#37cdbe',
-					neutral: '#3d4451',
+					primary: '#29395a',
+					secondary: '#d6a319',
+					accent: '#ffeeca',
+					neutral: '#877555',
 					'base-100': '#ffffff'
 				}
 			}
