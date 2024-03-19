94455abd5b710c58b5f600aea52897caa8acdeed
TheWanderingShinobi <thewanderingshinobi@proton.me>
2024-03-19 09:40:00 -0400

    deploy problem fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import critters from 'astro-critters';
-import sitemap from '@astrojs/sitemap';
-import robotsTxt from "astro-robots-txt";
-import partytown from "@astrojs/partytown";
-
-import compress from "astro-compress";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import critters from 'astro-critters'
+import sitemap from '@astrojs/sitemap'
+import robotsTxt from 'astro-robots-txt'
+import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://truenorthpropertyservices.ca',
-  integrations: [tailwind(), critters(), sitemap(), robotsTxt(), partytown(), compress()]
-});
+	site: 'https://truenorthpropertyservices.ca',
+	integrations: [tailwind(), critters(), sitemap(), robotsTxt(), partytown()]
+})
